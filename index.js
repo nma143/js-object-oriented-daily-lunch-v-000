015f5534d8b28dc848078a4d82dc94324080823d
Nikki A <nikki.apostolakis@gmail.com>
2019-04-23 16:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+class Neighborhood
+{
+  constructor (name)
+  {
+    this.id = ++neighborhoodId;
+    this.name = name;
+    store.neighborhoods.push(this)
+  }
